d8f2a1135ce1fe210a2ce4e27662205822ae7e12
Indymos <Indymos@github.com>
2023-02-04 13:38:33 +0330

    Add some changes.

--- PersianDateTime.cs
+++ PersianDateTime.cs
@@ -1,18 +1,24 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.Serialization;
 
 namespace System
 {
     public struct PersianDateTime
     {
 
-        private readonly DateTime dateTimeData;
-
+        private readonly DateTime _dateData;
         private PersianCalendar persianCalendar = new PersianCalendar();
+        private readonly DateTime MinValue = new DateTime(622, 3, 22);
 
+        public PersianDateTime()
+        {
+            _dateData = MinValue;
+        }
 
         public PersianDateTime(DateTime dateTime)
         {
-            dateTimeData = dateTime;
+            _dateData = (dateTime >= MinValue) ? dateTime : MinValue;
         }
 
         public PersianDateTime(int year, int month, int day) : this(year, month, day, 0, 0, 0, 0)
@@ -25,136 +31,141 @@ namespace System
 
         public PersianDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond)
         {
-            dateTimeData = new DateTime(year, month, day, hour, minute, second, millisecond, persianCalendar);
+            _dateData = new DateTime(year, month, day, hour, minute, second, millisecond, persianCalendar);
         }
 
 
-        public static PersianDateTime Now
+        public static PersianDateTime Now => new PersianDateTime(DateTime.Now);
+
+        public static PersianDateTime Today => Now.Date;
+
+        public PersianDateTime Date => new PersianDateTime(_dateData.Date);
+
+        public TimeSpan TimeOfDay => _dateData.TimeOfDay;
+
+        public long Ticks => _dateData.Ticks;
+
+        public int Millisecond => _dateData.Millisecond;
+        public int Second => _dateData.Second;
+        public int Minute => _dateData.Minute;
+        public int Hour => _dateData.Hour;
+        public int Day => persianCalendar.GetDayOfMonth(_dateData);
+        public int Month => persianCalendar.GetMonth(_dateData);
+        public int Year => persianCalendar.GetYear(_dateData);
+
+        public PersianDayOfWeek DayOfWeek => (PersianDayOfWeek)(int)_dateData.DayOfWeek;
+        public int DayOfYear => persianCalendar.GetDayOfYear(_dateData);
+
+        public int DaysInMonth => persianCalendar.GetDaysInMonth(Year, Month);
+        public int DaysInYear => persianCalendar.GetDaysInYear(Year);
+
+
+        public override string ToString()
         {
-            get
-            {
-                return new PersianDateTime(DateTime.Now);
-            }
+            return "";
         }
 
-        public static PersianDateTime Today
+
+        public PersianDateTime Add(TimeSpan value)
         {
-            get
-            {
-                return PersianDateTime.Now.Date;
-            }
+            return new PersianDateTime(_dateData.Add(value));
         }
 
-        public PersianDateTime Date
+        public PersianDateTime AddMilliseconds(double value)
         {
-            get
-            {
-                return new PersianDateTime(DateTime.Now.Date);
-            }
+            return new PersianDateTime(_dateData.AddMilliseconds(value));
         }
 
-        public TimeSpan TimeOfDay
+        public PersianDateTime AddSeconds(double value)
         {
-            get
-            {
-                return dateTimeData.TimeOfDay;
-            }
+            return new PersianDateTime(_dateData.AddSeconds(value));
         }
 
-
-        public int Millisecond
+        public PersianDateTime AddMinutes(double value)
         {
-            get
-            {
-                return (int)persianCalendar.GetMilliseconds(dateTimeData);
-            }
+            return new PersianDateTime(_dateData.AddMinutes(value));
         }
 
-        public int Second
+        public PersianDateTime AddHours(double value)
         {
-            get
-            {
-                return persianCalendar.GetSecond(dateTimeData);
-            }
+            return new PersianDateTime(_dateData.AddHours(value));
         }
 
-        public int Minute
+        public PersianDateTime AddDays(double value)
         {
-            get
-            {
-                return persianCalendar.GetMinute(dateTimeData);
-            }
+            return new PersianDateTime(_dateData.AddDays(value));
         }
 
-        public int Hour
+        public PersianDateTime AddMonths(int months)
         {
-            get
-            {
-                return persianCalendar.GetHour(dateTimeData);
-            }
+            return new PersianDateTime(_dateData.AddMonths(months));
         }
 
-        public int Day
+        public PersianDateTime AddYears(int value)
         {
-            get
-            {
-                return persianCalendar.GetDayOfMonth(dateTimeData);
-            }
+            return new PersianDateTime(_dateData.AddYears(value));
         }
-        public int Month
-        {
-            get
-            {
-                return persianCalendar.GetMonth(dateTimeData);
-            }
-        }
-
-        public int Year
-        {
-            get
-            {
-                return persianCalendar.GetYear(dateTimeData);
-            }
-        }
-
-        public PersianDayOfWeek DayOfWeek
-        {
-            get
-            {
-                return (PersianDayOfWeek)(int)dateTimeData.DayOfWeek;
-            }
-        }
-
-        public int DayOfYear
-        {
-            get
-            {
-                return persianCalendar.GetDayOfYear(dateTimeData);
-            }
-        }
-
-        public int DaysInMonth
-        {
-            get
-            {
-                return persianCalendar.GetDaysInMonth(Year, Month);
-            }
-        }
-
-        public int DaysInYear
-        {
-            get
-            {
-                return persianCalendar.GetDaysInYear(Year);
-            }
-        }
-
 
         public static int Compare(PersianDateTime t1, PersianDateTime t2)
         {
-            return 0;
+            return DateTime.Compare(t1._dateData, t2._dateData);
         }
 
+        public static bool IsLeapYear(int year)
+        {
+            PersianCalendar persian = new PersianCalendar();
+            return persian.IsLeapYear(year);
+        }
+
+
+        public static PersianDateTime operator +(PersianDateTime d, TimeSpan t)
+        {
+            return new PersianDateTime(d._dateData + t);
+        }
+
+        public static PersianDateTime operator -(PersianDateTime d, TimeSpan t)
+        {
+            return new PersianDateTime(d._dateData - t);
+        }
+
+        public static TimeSpan operator -(PersianDateTime d1, PersianDateTime d2) => new TimeSpan(d1.Ticks - d2.Ticks);
+
+        public static bool operator ==(PersianDateTime d1, PersianDateTime d2) => d1.Ticks == d2.Ticks;
+
+        public static bool operator !=(PersianDateTime d1, PersianDateTime d2) => d1.Ticks != d2.Ticks;
+
+        public static bool operator <(PersianDateTime t1, PersianDateTime t2) => t1.Ticks < t2.Ticks;
+
+        public static bool operator <=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks <= t2.Ticks;
+
+        public static bool operator >(PersianDateTime t1, PersianDateTime t2) => t1.Ticks > t2.Ticks;
+
+        public static bool operator >=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks >= t2.Ticks;
+
+
+        public override bool Equals([NotNullWhen(true)] object? value)
+        {
+            if (value is PersianDateTime)
+            {
+                return Ticks == ((PersianDateTime)value).Ticks;
+            }
+            return false;
+        }
+
+        public bool Equals(PersianDateTime value)
+        {
+            return Ticks == value.Ticks;
+        }
+
+        public static bool Equals(PersianDateTime t1, PersianDateTime t2)
+        {
+            return t1.Ticks == t2.Ticks;
+        }
+
+        public override int GetHashCode()
+        {
+            return _dateData.GetHashCode();
+        }
     }
 
     public enum PersianDayOfWeek
